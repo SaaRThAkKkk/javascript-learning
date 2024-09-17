d95d4d80796aab5d16c0518704127b426e2c4a89
Sarthak Dubey <sarthak0427@gmail.com>
2024-09-17 19:44:52 +0000

    data types and type casting

---
+++ basics/Notes.js
@@ -0,0 +1,20 @@
+/*
+1. Variale declaration
+    -> let,var,const and directly writng the variable name can be used for variable declaration.
+    -> we will use let and const var should be avoided and directly writing the variable name as well as it reduces the readibilty.
+    -> not assigining any value to any varaibler assigns it a undefined data value ny default
+
+2. Data Types IN JS
+    //we cant use alert in this terminala s we are using node js it runs int the browser engine only not in the node js
+    -> Numeric- to store the data in the numeric formats int,big_int.
+    -> String- " "
+    -> Boolean- true or false
+    -> NULL- stand alone value(representation of empty value) typeof(NULL)->object 
+    ,Undefined- value has not been yet assigned typeof(Undefined)-> undefined
+    ->symbol- basically used when we need to define the uniqueness
+
+3.Data Type Conversion 
+    -> 
+
+
+*/
